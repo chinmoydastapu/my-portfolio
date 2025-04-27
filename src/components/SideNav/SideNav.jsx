8fef619a8358fddd6f1a346e06d56783157e26d3
Chinmoy Das <chinmoydastapu2@gmail.com>
2025-04-27 14:37:09 +0600

    implement drawer navigation for smaller screens

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -7,72 +7,120 @@ import { IoPersonOutline, IoPricetagsOutline } from 'react-icons/io5';
 import { TfiWrite } from 'react-icons/tfi';
 import { AiOutlineMessage } from 'react-icons/ai';
 import { Link } from 'react-scroll';
+import { motion, AnimatePresence } from 'framer-motion';
 
-function SideNav() {
+function SideNav({ isDrawerOpen, closeDrawer }) {
+    const links = [
+        { to: "home", label: "home", icon: <TiHomeOutline className='w-6 h-6' /> },
+        { to: "expertise", label: "expertise", icon: <GiSkills className='w-6 h-6' /> },
+        { to: "portfolio", label: "portfolio", icon: <HiOutlineSquare3Stack3D className='w-6 h-6' /> },
+        { to: "resume", label: "resume", icon: <FaRegIdBadge className='w-6 h-6' /> },
+        { to: "clients", label: "clients", icon: <IoPersonOutline className='w-6 h-6' /> },
+        { to: "pricing", label: "pricing", icon: <IoPricetagsOutline className='w-6 h-6' /> },
+        { to: "blogs", label: "blogs", icon: <TfiWrite className='w-6 h-6' /> },
+        { to: "contact", label: "contact me", icon: <AiOutlineMessage className='w-6 h-6' /> },
+    ];
+
     const sideMenuStyles = 'flex items-center gap-3 transition-all ease-in-out cursor-pointer hover:text-[#ff6600] hover:scale-105';
     const followLinkStyles = 'w-12 h-12 p-3 rounded bg-[#1b1e22] shadow-custom cursor-pointer transition-all duration-300 ease-in-out hover:-translate-y-1 hover:bg-[#16181c] hover:text-[#ff6600]';
 
     return (
-        <div className="sticky top-0 left-0 hidden lg:block w-[290px] h-screen p-10 overflow-y-scroll scrollbar-hide bg-[#212428] uppercase font-playfair shrink-0">
-            <div className="h-52 w-52 mb-10 mx-auto rounded-full border-8 border-[#32363b] bg-cover" style={{ backgroundImage: `url(${profileImg})` }}></div>
-            <div className='flex flex-col gap-8 items-start'>
-                <Link to="home" smooth={true} duration={800} spy={true} activeClass="text-[#ff6600] scale-105">
-                    <div className={sideMenuStyles}>
-                        <TiHomeOutline className='w-6 h-6' />
-                        <span>home</span>
-                    </div>
-                </Link>
-                <Link to="expertise" smooth={true} duration={800} spy={true} activeClass="text-[#ff6600] scale-105">
-                    <div className={sideMenuStyles}>
-                        <GiSkills className='w-6 h-6' />
-                        <span>expertise</span>
-                    </div>
-                </Link>
-                <Link to="portfolio" smooth={true} duration={800} spy={true} activeClass="text-[#ff6600] scale-105">
-                    <div className={sideMenuStyles}>
-                        <HiOutlineSquare3Stack3D className='w-6 h-6' />
-                        <span>portfolio</span>
-                    </div>
-                </Link>
-                <Link to="resume" smooth={true} duration={800} spy={true} activeClass="text-[#ff6600] scale-105">
-                    <div className={sideMenuStyles}>
-                        <FaRegIdBadge className='w-6 h-6' />
-                        <span>resume</span>
-                    </div>
-                </Link>
-                <Link to="clients" smooth={true} duration={800} spy={true} activeClass="text-[#ff6600] scale-105">
-                    <div className={sideMenuStyles}>
-                        <IoPersonOutline className='w-6 h-6' />
-                        <span>clients</span>
-                    </div>
-                </Link>
-                <Link to="pricing" smooth={true} duration={800} spy={true} activeClass="text-[#ff6600] scale-105">
-                    <div className={sideMenuStyles}>
-                        <IoPricetagsOutline className='w-6 h-6' />
-                        <span>pricing</span>
-                    </div>
-                </Link>
-                <Link to="blogs" smooth={true} duration={800} spy={true} activeClass="text-[#ff6600] scale-105">
-                    <div className={sideMenuStyles}>
-                        <TfiWrite className='w-6 h-6' />
-                        <span>blogs</span>
-                    </div>
-                </Link>
-                <Link to="contact" smooth={true} duration={800} spy={true} activeClass="text-[#ff6600] scale-105">
-                    <div className={sideMenuStyles}>
-                        <AiOutlineMessage className='w-6 h-6' />
-                        <span>contact me</span>
-                    </div>
-                </Link>
+        <>
+            {/* Sidebar for large screens */}
+            <div className="hidden lg:block sticky top-0 left-0 w-[290px] h-screen p-10 overflow-y-scroll scrollbar-hide bg-[#212428] uppercase font-playfair shrink-0">
+                {/* Profile image */}
+                <div className="h-52 w-52 mb-10 mx-auto rounded-full border-8 border-[#32363b] bg-cover" style={{ backgroundImage: `url(${profileImg})` }}></div>
+                {/* Menu */}
+                <div className='flex flex-col gap-8 items-start'>
+                    {links.map(({ to, label, icon }, index) => (
+                        <Link
+                            key={index}
+                            to={to}
+                            smooth={true}
+                            duration={800}
+                            spy={true}
+                            activeClass="text-[#ff6600] scale-105"
+                            onClick={closeDrawer}
+                        >
+                            <div className={sideMenuStyles}>
+                                {icon}
+                                <span>{label}</span>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
+
+                {/* Divider and social icons */}
+                <div className='my-10 h-[1px] bg-[#32363b]'></div>
+                <h3 className='mb-5 font-semibold'>find me on</h3>
+                <div className='flex justify-center items-center gap-3'>
+                    <FaFacebookF className={followLinkStyles} />
+                    <FaWhatsapp className={followLinkStyles} />
+                    <FaLinkedinIn className={followLinkStyles} />
+                </div>
             </div>
-            <div className='my-10 h-[1px] bg-[#32363b]'></div>
-            <h3 className='mb-5 font-semibold'>find me on</h3>
-            <div className='flex justify-center items-center gap-3'>
-                <FaFacebookF className={followLinkStyles} />
-                <FaWhatsapp className={followLinkStyles} />
-                <FaLinkedinIn className={followLinkStyles} />
-            </div>
-        </div>
+
+            {/* Drawer for small screens */}
+            <AnimatePresence>
+                {isDrawerOpen && (
+                    <>
+                        {/* Backdrop */}
+                        <motion.div
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                            className="fixed inset-0 z-30 bg-black/40 lg:bg-transparent backdrop-blur-sm lg:backdrop-blur-0"
+                            onClick={closeDrawer}
+                        ></motion.div>
+
+                        {/* Drawer */}
+                        <motion.div
+                            initial={{ x: "-100%" }}
+                            animate={{ x: 0 }}
+                            exit={{ x: "-100%" }}
+                            transition={{ type: "tween", duration: 0.4 }}
+                            className="fixed top-0 left-0 z-40 w-[250px] h-full bg-[#212428] p-6 overflow-y-auto scrollbar-hide flex flex-col uppercase font-playfair lg:hidden"
+                        >
+
+                            {/* Profile image */}
+                            <div
+                                className="min-h-40 min-w-40 mb-10 mx-auto rounded-full border-4 border-[#32363b] bg-cover bg-center bg-no-repeat"
+                                style={{ backgroundImage: `url(${profileImg})` }}
+                            ></div>
+
+                            {/* Menu */}
+                            <div className="flex flex-col gap-6 items-start">
+                                {links.map(({ to, label, icon }, index) => (
+                                    <Link
+                                        key={index}
+                                        to={to}
+                                        smooth={true}
+                                        duration={800}
+                                        spy={true}
+                                        activeClass="text-[#ff6600] scale-105"
+                                        onClick={closeDrawer}
+                                    >
+                                        <div className={sideMenuStyles}>
+                                            {icon}
+                                            <span>{label}</span>
+                                        </div>
+                                    </Link>
+                                ))}
+                            </div>
+
+                            {/* Divider and social icons */}
+                            <div className="my-10 min-h-[1px] bg-[#32363b]"></div>
+                            <h3 className="mb-5 font-semibold">find me on</h3>
+                            <div className="flex justify-center items-center gap-3">
+                                <FaFacebookF className={followLinkStyles} />
+                                <FaWhatsapp className={followLinkStyles} />
+                                <FaLinkedinIn className={followLinkStyles} />
+                            </div>
+                        </motion.div>
+                    </>
+                )}
+            </AnimatePresence>
+        </>
     );
 }
 
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,9 +4,11 @@ import SideNav from "../components/SideNav/SideNav";
 import { Outlet } from "react-router-dom";
 import { FaAnglesUp } from "react-icons/fa6";
 import { Link } from "react-scroll";
+import { FaChevronRight } from "react-icons/fa";
 
 function MainLayout() {
     const [showUpButton, setShowUpButton] = useState(false);
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -22,14 +24,29 @@ function MainLayout() {
     }, []);
 
     return (
-        <div className="flex">
-            <SideNav />
+        <div className="flex relative">
+            {/* Menu button for small screens */}
+            <button
+                onClick={() => setIsDrawerOpen(true)}
+                className={`fixed top-5 left-5 z-50 bg-[#212428] text-white p-3 rounded-md shadow-custom ${isDrawerOpen ? 'hidden' : 'block'} lg:hidden`}
+            >
+                <FaChevronRight className="w-6 h-6" />
+            </button>
+
+            <SideNav isDrawerOpen={isDrawerOpen} closeDrawer={() => setIsDrawerOpen(false)} />
+
             <div className="grow">
                 <Outlet />
                 <Footer />
             </div>
+
             {showUpButton && (
-                <Link to="home" smooth={true} duration={1000} className="fixed right-10 bottom-10 px-5 py-3 rounded bg-[#1b1e22] uppercase font-semibold shadow-custom cursor-pointer transition-all duration-300 ease-in-out hover:-translate-y-1 hover:bg-[#16181c] hover:text-[#ff6600]">
+                <Link
+                    to="home"
+                    smooth={true}
+                    duration={1000}
+                    className="fixed right-10 bottom-10 px-5 py-3 rounded bg-[#1b1e22] uppercase font-semibold shadow-custom cursor-pointer transition-all duration-300 ease-in-out hover:-translate-y-1 hover:bg-[#16181c] hover:text-[#ff6600]"
+                >
                     <FaAnglesUp className="w-6 h-6" />
                 </Link>
             )}
